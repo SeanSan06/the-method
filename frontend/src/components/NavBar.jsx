fd16622d7162bc9ab5184d4f9a022e21cbd268bb
Sean San <seanksan@gmail.com>
2026-01-19 01:02:13 -0800

    Create basic navigation bar and configure page

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+    return  (
+        <nav>
+            <h1>The Method</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
