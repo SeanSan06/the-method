fc815c951d76eb058e7a4cb733d7964f414e4407
Sean San <seanksan@gmail.com>
2026-01-19 02:31:58 -0800

    Reformat, color the nav bar, & design acc buttons

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,9 +4,14 @@ function NavBar() {
             <h1 id="the-method">The Method</h1>
             <ul id="nav-bar-links">
                 <li>Home</li>
+                <li>Dashboard</li>
                 <li>About</li>
                 <li>Contact</li>
             </ul>
+            <div id="log-in-sign-up">
+                <button>Log in</button>
+                <button>Sign up</button>
+            </div>
         </nav>
     );
 }
