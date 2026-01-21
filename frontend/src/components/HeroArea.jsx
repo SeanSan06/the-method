09ac8a496209742b6bc83a16921dd02e04b9c3c7
Sean San <seanksan@gmail.com>
2026-01-20 23:58:37 -0800

    Add a "make resume" button onto the home page

--- frontend/src/components/HeroArea.jsx
+++ frontend/src/components/HeroArea.jsx
@@ -3,6 +3,7 @@ function HeroArea() {
         <div id="hero-area">
             <h1>The <span id="method-word">Method</span></h1>
             <p>Make Every Task Happen, Own Destiny</p>
+            <button id="make-resume">Make Resume</button>
         </div>
     );
 }
