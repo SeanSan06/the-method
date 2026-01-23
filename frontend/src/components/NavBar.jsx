78ec78456af6709c65d88131f4537500d4b31cf2
Sean San <seanksan@gmail.com>
2026-01-22 22:50:20 -0800

    Apply underline to nav bar links
    The nav bar elements will update according to the page that the user
    is on. For now only the home page and resume have this feautre.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
 
 function NavBar() {
     const navigate = useNavigate()
@@ -7,9 +9,28 @@ function NavBar() {
         <nav href="/" id="nav-bar">
             <h2 id="the-method">The Method</h2>
             <ul id="nav-bar-links">
-                <li onClick={() => navigate('/')}>Home</li>
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) => isActive ? 'active-link' : undefined}
+                    >
+                        Home
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink
+                        to="/form"
+                        className={({ isActive }) => isActive ? 'active-link' : undefined}
+                    >
+                        Form
+                    </NavLink>
+                </li>
+
                 <li>Dashboard</li>
+                
                 <li>About</li>
+
                 <li>Contact</li>
             </ul>
             <div id="log-in-sign-up">
