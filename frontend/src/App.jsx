1d6dbf3fa099ccb3b84099880061034b2d681ecf
Sean San <seanksan@gmail.com>
2026-01-27 22:16:28 -0800

    Fix paths for about & contact, make nav txt bigger
    The about and contact section lead to the dashboard webpage. Now they
    properly link to their own webpage.
    
    This also increased the text size of the h2 "The Method" and the log in
    and sign up buttons that are in the navigation bar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,15 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import FormPage from  './pages/FormPage';
 import DashboardPage from  './pages/DashboardPage';
+import AboutPage from  './pages/AboutPage';
+import ContactPage from  './pages/ContactPage';
 
-import './styles/styles.css'
+import './styles/styles.css' 
 import './styles/home-page.css'
 import './styles/form-page.css'
 import './styles/dashboard-page.css'
+import './styles/about-page.css'
+import './styles/contact-page.css'
 
 // Defines what Page Component appears for each of the 5 webpages
 function App() {
@@ -18,8 +22,8 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/form" element={<FormPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/about" element={<DashboardPage />} />
-            <Route path="/contact" element={<DashboardPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
         </Routes>
     </div>
     )
