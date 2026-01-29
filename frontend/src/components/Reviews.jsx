81bc19c3f91c83d06b3f19cd886244858ea0bda5
Sean San <seanksan@gmail.com>
2026-01-28 19:32:58 -0800

    Layout basic positions for review area component

---
+++ frontend/src/components/Reviews.jsx
@@ -0,0 +1,34 @@
+function Reviews() {
+    return  (
+        <div>
+            <div id="reviews-area">
+                <h2 id="review-header">Reviews</h2>
+                <p id="review-caption">Hear from succesful other users</p>
+                <div id="review-cards-area">
+                    <div id="review-card-1">
+                        <p>5 Stars</p>
+                        <p>"I love this website so much!"</p>
+                        <p>Coda K Litel</p>
+                        <p>SWE Intern at XYZ</p>
+                    </div>
+                    
+                    <div id="review-card-2">
+                        <p>4.5 Stars</p>
+                        <p>"I just got into FANNG"</p>
+                        <p>Sunil</p>
+                        <p>20x Developer at </p>
+                    </div>
+
+                    <div id="review-card-3">
+                        <p>4.75 Stars</p>
+                        <p>"One of the best out there"</p>
+                        <p>Turring</p>
+                        <p>Founder of tech startup</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Reviews;
