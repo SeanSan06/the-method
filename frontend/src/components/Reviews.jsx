7058241add8e54fe30bbf91a9c789852671b1519
Sean San <seanksan@gmail.com>
2026-01-28 20:15:04 -0800

    Realign and resize all review area elements

--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -7,23 +7,33 @@ function Reviews() {
                 <div id="review-cards-area">
                     <div id="review-card-1">
                         <p>5 Stars</p>
-                        <p>"I love this website so much!"</p>
-                        <p>Coda K Litel</p>
-                        <p>SWE Intern at XYZ</p>
+                        <p class="reivew-quote">"I love this website so much! The Method helped me land
+                            a SWE intern position at my dream company!"</p>
+                        <div>
+                            <p class="reivew-name">-Coda K Litel</p>
+                            <p class="reivew-job">SWE Intern at XYZ</p>
+                        </div>
                     </div>
-                    
+
                     <div id="review-card-2">
                         <p>4.5 Stars</p>
-                        <p>"I just got into FANNG"</p>
-                        <p>Sunil</p>
-                        <p>20x Developer at </p>
+                        <p class="reivew-quote">"I just got into FANNG, I am so glad that
+                            The Method exists. It has truly been an amazing tool for my
+                            job search."</p>
+                        <div>
+                            <p class="reivew-name">-Sunil</p>
+                            <p class="reivew-job">20x Engineer at Google</p>
+                        </div>
                     </div>
 
                     <div id="review-card-3">
                         <p>4.75 Stars</p>
-                        <p>"One of the best out there"</p>
-                        <p>Turring</p>
-                        <p>Founder of tech startup</p>
+                        <p class="reivew-quote">"One of the best job prep websites out there.
+                            I cant believe this is free."</p>
+                        <div>
+                            <p class="reivew-name">-Turring</p>
+                            <p class="reivew-job">Founder of tech startup</p>
+                        </div>
                     </div>
                 </div>
             </div>
