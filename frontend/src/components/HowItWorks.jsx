18943be0211059d7671303fb84192a4cc00fd648
Lien Jabujab <lbjabujab58@gmail.com>
2026-01-28 23:51:31 -0800

    Fix Position issues with HowItWorks
    -Fixed the position of upper and lower box to make the design
    consistent.

--- frontend/src/components/HowItWorks.jsx
+++ frontend/src/components/HowItWorks.jsx
@@ -20,36 +20,42 @@ function HowItWorks() {
                             Instant grading and tips to improve your resume with AI-powered analysis.
                         </p>
                     </div>
-                    <h2>
-                        02
-                    </h2>
-                    <h2>
-                        Interview Prep
-                    </h2>
-                    <p>
-                        Practice questions and get expert advice to ace your interviews.
-                    </p>
+                    <div id = "box-two">
+                        <h2>
+                            02
+                        </h2>
+                        <h2>
+                            Interview Prep
+                        </h2>
+                        <p>
+                            Practice questions and get expert advice to ace your interviews.
+                        </p>
+                    </div>
                 </div>
 
                 <div id = "lower-box">
-                    <h2>
-                        03
-                    </h2>
-                    <h2>
-                        Job Applications
-                    </h2>
-                    <p>
-                        Apply directly to jobs through our platform with one click.
-                    </p>
-                    <h2>
-                        04
-                    </h2>
-                    <h2>
-                        Progress Tracking
-                    </h2>
-                    <p>
-                        See how close you are to landing your dream job with analytics. 
-                    </p>
+                    <div>
+                        <h2>
+                            03
+                        </h2>
+                        <h2>
+                            Job Applications
+                        </h2>
+                        <p>
+                            Apply directly to jobs through our platform with one click.
+                        </p>
+                    </div>
+                    <div>
+                        <h2>
+                            04
+                        </h2>
+                        <h2>
+                            Progress Tracking
+                        </h2>
+                        <p>
+                            See how close you are to landing your dream job with analytics.
+                        </p>
+                    </div>
 
                 </div>
 
