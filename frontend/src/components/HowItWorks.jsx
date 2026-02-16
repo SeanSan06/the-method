5f11d3b1057e6d04266d04f853e9213acc7d1f22
Lien Jabujab <lbjabujab58@gmail.com>
2026-02-16 00:09:01 -0800

    Add skyblue to howitworks captio and "Method"
    -Only put skyblue in "Method" for the title "How the Method Works"
    -Experimented with differing colors
    -Will add a reaction when user hovers to boxes in future.

--- frontend/src/components/HowItWorks.jsx
+++ frontend/src/components/HowItWorks.jsx
@@ -2,17 +2,17 @@ function HowItWorks() {
     return (
         <div>
                 <h1 id="how-it-works-title"> 
-                    How The Method Works 
+                    How The <span id = "how-it-works-method-section">Method</span> Works 
                 </h1>
                 <p id="how-it-works-caption">
                     Four simple steps to your next career move
                 </p>
             <div id="whole-box">
                 <div className = "steps-box">
-                    <h2>
+                    <h2 class = "box-number">
                         01
                     </h2>
-                    <h2>
+                    <h2 class = "box-title">
                         Resume Upload & Feedback
                     </h2>
                     <p>
@@ -21,10 +21,10 @@ function HowItWorks() {
                 </div>
 
                 <div className = "steps-box">
-                    <h2>
+                    <h2 class = "box-number">
                         02
                     </h2>
-                    <h2>
+                    <h2 class = "box-title">
                         Interview Prep
                     </h2>
                     <p>
@@ -33,10 +33,10 @@ function HowItWorks() {
                 </div>
             
                 <div className = "steps-box">
-                    <h2>
+                    <h2 class = "box-number">
                         03
                     </h2>
-                    <h2>
+                    <h2 class = "box-title">
                         Job Applications
                     </h2>
                     <p>
@@ -45,10 +45,10 @@ function HowItWorks() {
                 </div>
 
                 <div className = "steps-box">
-                    <h2>
+                    <h2 class = "box-number">
                         04
                     </h2>
-                    <h2>
+                    <h2 class = "box-title">
                         Progress Tracking
                     </h2>
                     <p>
