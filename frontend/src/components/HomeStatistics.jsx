2f3dec90e8d472885986e1a74f5f2ce437a96833
Sean San <seanksan@gmail.com>
2026-02-22 01:34:06 -0800

    feat(Frontend): Update statistic area on home page to be spaced out more

--- frontend/src/components/HomeStatistics.jsx
+++ frontend/src/components/HomeStatistics.jsx
@@ -1,43 +1,48 @@
 function HomeStatistics() {
     return  (
         <div id="home-statistics-area">
-            <h2 id="home-statistics-title-h2">Real Success</h2>
-            
-            <div id="home-users-count">
-                <h3>Users</h3>
-                <p>0</p>
+            <div id="home-users-count-subarea">
+                <h2 id="home-statistics-title-h2">Real Success</h2>
+                <div id="home-users-count">
+                    <h4>Users</h4>
+                    <p>0</p>
+                </div>
             </div>
 
             <div id="home-statistics">
-                <h3 id="home-impact-statistics-subarea">Impact Statistics</h3>
-                <div id="impact-statistics-data">
-                    <div id="home-resumes-sent">
-                        <h4>Resumes Sent</h4>
-                        <p>0</p>
-                    </div>
-                    <div id="home-interviews-held">
-                        <h4>Interviews Held</h4>
-                        <p>0</p>
-                    </div>
-                    <div id="home-jobs-offered">
-                        <h4>Jobs Offered</h4>
-                        <p>0</p>
+                <div id="home-impact-statistics-subarea">
+                    <h3 id="home-impact-statistics-h3">Impact Statistics</h3>
+                    <div id="impact-statistics-data">
+                        <div id="home-resumes-sent">
+                            <h4>Resumes Sent</h4>
+                            <p>0</p>
+                        </div>
+                        <div id="home-interviews-held">
+                            <h4>Interviews Held</h4>
+                            <p>0</p>
+                        </div>
+                        <div id="home-jobs-offered">
+                            <h4>Jobs Offered</h4>
+                            <p>0</p>
+                        </div>
                     </div>
                 </div>
 
-                <h3 id="home-the-method-statistics-subarea">The Method Statistics</h3>
-                <div id="home-the-method-statistics-data">
-                    <div id="home-resumes-optimized">
-                        <h4>Resumes Optimized</h4>
-                        <p>0</p>
-                    </div>
-                    <div id="home-hours-practicing-interviews">
-                        <h4>Hours Practicing Interviews</h4>
-                        <p>0</p>
-                    </div>
-                    <div id="home-jobs-found">
-                        <h4>Jobs Recommended</h4>
-                        <p>0</p>
+                <div id="home-impact-statistics-subarea">
+                    <h3 id="home-the-method-statistics-h3">The Method Statistics</h3>
+                    <div id="home-the-method-statistics-data">
+                        <div id="home-resumes-optimized">
+                            <h4>Resumes Optimized</h4>
+                            <p>0</p>
+                        </div>
+                        <div id="home-hours-practicing-interviews">
+                            <h4>Hours Practicing Interviews</h4>
+                            <p>0</p>
+                        </div>
+                        <div id="home-jobs-found">
+                            <h4>Jobs Recommended</h4>
+                            <p>0</p>
+                        </div>
                     </div>
                 </div>
             </div>
