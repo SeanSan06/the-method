a669d40a731b53b5e5686099d54e7a1a36c50175
Sean San <seanksan@gmail.com>
2026-02-21 23:58:48 -0800

    feat(Frontend): Layout the basic HTML structure for the home pg stats
    There are 2 sections, one for success stats and the other of website
    stats. The stats are currently hardcoded, but will be dynamic when
    the database is built out more.

--- frontend/src/components/HomeStatistics.jsx
+++ frontend/src/components/HomeStatistics.jsx
@@ -1,7 +1,38 @@
 function HomeStatistics() {
     return  (
-        <div>
-            {/*  */}
+        <div id="home-statistics-area">
+            <h2>Home Statistics</h2>
+            <div id="home-users-count">
+                <h3>Users</h3>
+                <p>0</p>
+            </div>
+            <div id="home-statistics">
+                <h3>Impact Statistics</h3>
+                <div>
+                    <div>
+                        <h3>ATS Optimized</h3>
+                    </div>
+                    <div>
+                        <h3>Jobs Landed</h3>
+                    </div>
+                    <div>
+                        <h3>Hours Saved</h3>
+                    </div>
+                </div>
+
+                <h3>The Method Statistics</h3>
+                <div>
+                    <div>
+                        <h3>Resumes Optimize</h3>
+                    </div>
+                    <div>
+                        <h3>Hours Practicing Inteviews</h3>
+                    </div>
+                    <div>
+                        <h3>Jobs Applied</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import HeroArea from "../components/HeroArea";
 import HowItWorks from "../components/HowItWorks";
+import HomeStatistics from "../components/HomeStatistics";
 import Reviews from "../components/Reviews";
 import Footer from "../components/Footer";
 
@@ -8,6 +9,7 @@ function HomePage() {
         <div>
             <HeroArea />
             <HowItWorks />
+            <HomeStatistics />
             <Reviews />
             <Footer />
         </div>
