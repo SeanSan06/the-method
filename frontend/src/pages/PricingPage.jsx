3a4356723b451788d9617526965c10bc56042f66
Sean San <seanksan@gmail.com>
2026-02-23 13:00:50 -0800

    feat(Frontend): Layout the pricing page using flexbox
    This puts all 3 tiers into a box and lays them out horizontally.

--- frontend/src/pages/PricingPage.jsx
+++ frontend/src/pages/PricingPage.jsx
@@ -2,47 +2,51 @@ function PricingPage() {
     return  (
         <div id="pricing-page">
             <h1 id="pricing-page-h1">Pricing Page</h1>
-            <div>
-                <h2>Free</h2>
-                <p>Basic features for everyone</p>
-                <p>$0/month</p>
-                <div>
-                    <ul>
-                        <li>Basic access to all features</li>
-                        <li>Community support</li>
-                    </ul>
+            <div id="pricing-tiers-subarea">
+                <div id="pricing-free-tier">
+                    <h2>Free</h2>
+                    <p>Basic features for everyone</p>
+                    <p>$0/month</p>
+                    <div>
+                        <ul>
+                            <li>Basic access to all features</li>
+                            <li>Community support</li>
+                            <li>Interview Prep</li>
+                            <li>Job Apply</li>
+                            <li>WIP</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-
-            <div>
-                <h2>Student</h2>
-                <p>Discounted pricing for students</p>
-                <p>$2.49/month</p>
-                <div>
-                    <ul>
-                        <li>Everything in free plan +</li>
-                        <li>Store 10 more resumes</li>
-                        <li>Priority support</li>
-                        <li>Feature requests</li>
-                        <li>WIP</li>
-                    </ul>
+                <div id="pricing-student-tier">
+                    <h2>Student</h2>
+                    <p>Discounted pricing for students</p>
+                    <p>$2.49/month</p>
+                    <div>
+                        <ul>
+                            <li>Everything in free plan +</li>
+                            <li>Store 10 more resumes</li>
+                            <li>Priority support</li>
+                            <li>Feature requests</li>
+                            <li>WIP</li>
+                        </ul>
+                    </div>
                 </div>
-
-            </div>
-
-            <div>
-                <h2>Premium</h2>
-                <p>All features for professionals</p>
-                <p>$6.99/month</p>
-                <div>
-                    <ul>
-                        <li>Everything in free plan +</li>
-                        <li>WIP</li>
-                    </ul>
+                <div id="pricing-premium-tier">
+                    <h2>Premium</h2>
+                    <p>All features for professionals</p>
+                    <p>$6.99/month</p>
+                    <div>
+                        <ul>
+                            <li>Everything in free plan +</li>
+                            <li>Store 15 more resumes</li>
+                            <li>Use more optimization options</li>
+                            <li>5 more resume templates</li>
+                            <li>WIP</li>
+                        </ul>
+                    </div>
                 </div>
-
+                        </div>
             </div>
-        </div>
     );
 }
 
