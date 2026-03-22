27beda67fefbf3cddf92bcfa94ea78f62f8519cb
Sean San <seanksan@gmail.com>
2026-03-21 23:57:46 -0700

    feat: Create data-page.css and layout basic content for DataPage.jsx
    This just outlines the basic privacy policy content/rues for the
    DataPage.jsx page of the website. The CSS file is empty for now, but
    will be used to style the page in the future.

--- frontend/src/pages/DataPage.jsx
+++ frontend/src/pages/DataPage.jsx
@@ -1,9 +1,19 @@
 function Data() {
     return  (
         <div id="data">
-                <h1>Your Data</h1>
-                <p>Manage your data and privacy settings.</p>
-                <p>Here you can view and manage the data we have collected about you, as well as adjust your privacy settings and preferences.</p>
+            <a href="/">Back Home</a>
+                <h1>Our Privacy Policy</h1>
+                <p>
+                    Last updated: <time dateTime="2026-03-21">March 21, 2026</time>
+                </p>
+
+                <ol>
+                    <li>Here at The Method we respect your data and privacy when applying to jobs.</li>
+                    <li>We collect your name email address for account purposes.</li>
+                    <li>We do not sell your data ever.</li>
+                    <li>Resumes will be stored in our databases, but you can always remove them.</li>
+                </ol>
+
         </div>
     );
 }
