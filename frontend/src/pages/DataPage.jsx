e2ef53068c2e5f3c9cf1a6eee947a6e51fb05c65
Sean San <seanksan@gmail.com>
2026-03-22 20:09:44 -0700

    fix: Touch up data page styling and a back to home link

--- frontend/src/pages/DataPage.jsx
+++ frontend/src/pages/DataPage.jsx
@@ -1,18 +1,24 @@
+import { Link } from "react-router-dom";
+
 function Data() {
     return  (
         <div id="data-page">
             <div id="data-content">
-                <a href="/">Back Home</a>
-                    <h1>Our Privacy Policy</h1>
-                    <p>
-                        Last updated: <time dateTime="2026-03-22">March 22, 2026</time>
-                    </p>
-                    <ol>
+                <Link to="/" id="back-home-link">
+                    Back Home
+                </Link>
+                <h1>Our Privacy Policy</h1>
+                <p>
+                    Last updated: <time dateTime="2026-03-22">March 22, 2026</time>
+                </p>
+                <ol>
                         <li>Here at The Method we respect your data and privacy when applying to jobs.</li>
                         <li>We collect your name email address for account purposes.</li>
                         <li>We do not sell your data ever.</li>
                         <li>Resumes will be stored in our databases, but you can always remove them.</li>
-                    </ol>
+                </ol>
+                
+                <p><strong>Disclaimer:</strong> Subject to update as we build our website. Not to worry, we will always prioritize your privacy.</p>
             </div>
         </div>
     );
